bad893013a62a57124a6b0f62c60305808d52a74
Yassine Boumajane <yassine_boumajane@carrefour.com>
2020-09-20 19:17:14 +0200

    Configuration Automation

---
+++ scripts/down.sh
@@ -0,0 +1,2 @@
+az login --service-principal -u app_id -p secret_id --tenant tenant_id
+az vm stop -g resource_group -n vm_name
---
+++ scripts/up.sh
@@ -0,0 +1,2 @@
+az login --service-principal -u app_id -p secret_id --tenant tenant_id
+az vm start -g resource_group -n vm_name
